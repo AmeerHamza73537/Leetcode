94e2f416c91a8056db4ea1339a905a2fe43c248c
Ameer Hamza <zarb55091@gmail.com>
2025-06-15 22:26:01 +0500

    Time: 67 ms (21.51%), Space: 92.9 MB (17.9%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> ans(nums.begin(), nums.end());
+
+        return ans.size() < nums.size();
+    }
+};
