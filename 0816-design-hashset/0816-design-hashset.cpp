2801dea29e6708f65f7028ec9948ea54c934c829
Ameer Hamza <zarb55091@gmail.com>
2025-07-22 23:33:04 +0500

    Time: 7 ms (93.04%), Space: 50.3 MB (41.85%) - LeetHub

---
+++ 0816-design-hashset/0816-design-hashset.cpp
@@ -0,0 +1,29 @@
+class MyHashSet {
+public:
+    static const int SIZE = 1e6 + 1;
+    vector<bool> data;
+
+    MyHashSet() {
+        data = vector<bool>(SIZE, false);
+    }
+
+    void add(int key) {
+        data[key] = true;
+    }
+
+    void remove(int key) {
+        data[key] = false;
+    }
+
+    bool contains(int key) {
+        return data[key]; 
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
